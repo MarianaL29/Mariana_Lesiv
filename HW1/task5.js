ea1407ac4a4e11f5ff6ece46f1687c235cbf6787
MarianaL29 <mar.vlesiv@gmail.com>
2021-03-11 21:31:59 +0200

    Add tasks description

--- HW1/task5.js
+++ HW1/task5.js
@@ -1,3 +1,7 @@
-age = prompt('How old are you?')
-question = confirm ('Are you ' + age+ '?')
+// 5. Запросите у пользователя его возраст, после чего выведите на экран модальное окно с 
+// вопросом: "Ваш возраст ...(введеное пользователем число) лет?". 
+// Далее в зависимости от выбора пользователя выводится модальное окно с информацией true или false.
+
+var age = prompt('How old are you?')
+var question = confirm ('Are you ' + age+ '?')
 alert (question)
