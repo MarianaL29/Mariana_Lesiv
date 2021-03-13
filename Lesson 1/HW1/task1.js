f49ad0453be81b880e3669c97ca58edc8902e7c1
MarianaL29 <mar.vlesiv@gmail.com>
2021-03-13 16:02:13 +0200

    Update task1.js

--- Lesson 1/HW1/task1.js
+++ Lesson 1/HW1/task1.js
@@ -3,11 +3,9 @@
 //    Общий вид уравнения: y = kx + b; где k = (y1 - y2) / (x1 - x2), b = y2 - k*x2.
 
 let y, k, b, y1, y2, x1 ,x2;
-   
-
  x1 = prompt('Please enter x1');
  x2 = prompt('Please enter x2');
- y1 = prompt('Please enter y1');
+ y1 = prompt('Please enter y1')
  y2 = prompt('Please enter y2');
 
     k = (y1 - y2) / (x1 - x2);
